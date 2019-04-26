500cdc547988b484a232f7302aeacbb9a0406dee
Emilio Manrique <conandoel@gmail.com>
2019-04-25 23:07:17 -0300

    Se crearon actividades en los botones

--- app/src/main/java/com/example/ciclodevida/MainActivity.java
+++ app/src/main/java/com/example/ciclodevida/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.ciclodevida;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botonSaludar, botonReset;
+    private EditText editNombre;
+    private TextView textSaludo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonSaludar = findViewById(R.id.botonSaludar);
+        botonReset = findViewById(R.id.botonReset);
+        editNombre = findViewById(R.id.editNombre);
+        textSaludo = findViewById(R.id.textSaludo);
+    }
+
+    public void saludar(View v){
+        if(editNombre.getText().toString().equals("")){
+            Toast.makeText(this, "Debe ingresar un Nombre", Toast.LENGTH_SHORT).show();
+            //Log.i("saludo", "saludar: ");
+        }
+        else {
+            //el usuario ingresa su nombre
+            textSaludo.setText("Bienvenido " + editNombre.getText().toString());
+            botonReset.setEnabled(true);
+        }
+
+    }
+
+    public void reset(View v){
+        textSaludo.setText("");
+        editNombre.setText("");
+        botonReset.setEnabled(false);
+
     }
 }
